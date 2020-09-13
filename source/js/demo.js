30888f9d29350a3209bc06abfedd0f88b0aa16f5
NikitaSech <NikitaSech@yandex.ru>
2020-09-13 23:39:07 +0300

    Исправляет ошибки npm test

--- source/js/demo.js
+++ source/js/demo.js
@@ -1,17 +1,17 @@
 (function () {
-	var slider = window.slider;
+  var slider = window.slider;
 
-	var demoElement = document.querySelector('.demo');
-	var sliderElement = demoElement.querySelector('.demo__slider');
+  var demoElement = document.querySelector('.demo');
+  var sliderElement = demoElement.querySelector('.demo__slider');
   var sliderPinElement = sliderElement.querySelector('.slider__button');
   var sliderToogleLeft = sliderElement.querySelector('.slider__toggle--left');
   var sliderToogleRight = sliderElement.querySelector('.slider__toggle--right');
 
   function demoSlid(percent) {
-  	demoBeforeImgElement = demoElement.querySelector('.demo__img-container--before');
+    demoBeforeImgElement = demoElement.querySelector('.demo__img-container--before');
     demoAfterImgElement = demoElement.querySelector('.demo__img-container--after');
-  	var pictureWidth = 100 - percent;
-  	demoBeforeImgElement.style.width = `${pictureWidth}%`;
+    var pictureWidth = 100 - percent;
+    demoBeforeImgElement.style.width = `${pictureWidth}%`;
   }
 
   function zeroizeDemo() {
@@ -20,17 +20,17 @@
 
     slider.zeroizePin(sliderElement, demoSlid);
     setTimeout(function () {
-    	beforeImgElement.style.transition = `none`;
+      beforeImgElement.style.transition = `none`;
     }, 400);
   }
 
   function fillDemo() {
-  	var beforeImgElement = demoElement.querySelector('.demo__img-container--before');
+    var beforeImgElement = demoElement.querySelector('.demo__img-container--before');
     beforeImgElement.style.transition = `width ease-in-out 0.4s`;
 
     slider.fillPin(sliderElement, demoSlid);
     setTimeout(function () {
-    	beforeImgElement.style.transition = `none`;
+      beforeImgElement.style.transition = `none`;
     }, 400);
   }
 
@@ -47,30 +47,30 @@
   }
 
   function onLeftToggleClick(evt) {
-  	evt.preventDefault();
+    evt.preventDefault();
 
     zeroizeDemo();
   }
 
   function onLeftTogglePressEnter(evt) {
-  	evt.preventDefault();
+    evt.preventDefault();
 
     if (evt.code === 'Enter') {
-    	zeroizeDemo();
+      zeroizeDemo();
     }
   }
 
   function onRightToggleClick(evt) {
-  	evt.preventDefault();
+    evt.preventDefault();
 
     fillDemo();
   }
 
   function onRightTogglePressEnter(evt) {
-  	evt.preventDefault();
+    evt.preventDefault();
 
     if (evt.code === 'Enter') {
-    	fillDemo();
+      fillDemo();
     }
   }
 
